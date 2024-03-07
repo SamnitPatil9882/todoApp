69dcca63ee35ab9565d762592f3f6e998f4503f7
Samnit Patil <samnitpatil9882@gmail.com>
2024-03-07 14:48:32 +0530

    assignment 3 completion

--- src/components/inputTodo.tsx
+++ src/components/inputTodo.tsx
@@ -1,27 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function InputDiv({ addTodoData }: { addTodoData: (title: string, description: string) => void }) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isSubmited, setIsSubmited] = useState(false);
+    const [error, setError] = useState<string>("")
 
     const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTitle(event.target.value);
+        setIsSubmited(false)
+        setError("")
     }
 
     const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setDescription(event.target.value);
+        setIsSubmited(false)
+        setError("")
     }
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setIsSubmitting(true);
+        fetch("http://localhost:8000/todos",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    title,
+                    description,
+                }),
+            }
+        )
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                setError("");
+                setIsSubmited(true)
+            })
+            .catch(error => setError(error))
+
+        
         addTodoData(title, description);
         setTitle("");
         setDescription("");
-        setIsSubmitting(false);
     }
-    
+
     return (
         <div className="h-25 container-lg mx-auto bg-slate-800 flex justify-center items-center">
             <form className="flex flex-col items-center" onSubmit={handleSubmit}>
@@ -47,9 +68,10 @@ function InputDiv({ addTodoData }: { addTodoData: (title: string, description: s
                     className="placeholder-italic placeholder-slate-400 text-black rounded-md h-10 p-2 w-full m-2"
                 />
 
-                {isSubmitting && <div className="text-green-500">Submitting data...</div>}
-
+                {isSubmited && <div className="text-green-500">Submited data successfully</div>}
+                {error && <div className="text-red-500"   >Internal Server Error</div>}
                 <button type="submit" className="w-20 m-2 bg-green-400 rounded-md h-10 p-2 ml-2">+ Todo</button>
+                
             </form>
         </div>
     )
